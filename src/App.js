36fb068453d7024b97d53606c16ba6c056048834
Dani Garrincha <daniyar0303@gmail.com>
2024-04-03 15:30:18 +0600

    finnal commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import WeatherComponent from "./components/WeatherComponent";
 
 function App() {
   return (
-    <div className=" min-h-screen bg-gradient-to-r from-rose-400 to-orange-300 flex flex-col justify-center items-center">
+    <div className="min-h-screen bg-gradient-to-r from-rose-400 to-orange-300 flex flex-col justify-center items-center overflow-x-hidden">
       <div className=" bg-opacity-10 bg-white px-4 py-6 rounded-lg mt-2 mb-2">
         <WeatherComponent />
       </div>
--- src/components/WeatherComponent.jsx
+++ src/components/WeatherComponent.jsx
@@ -3,10 +3,13 @@ import { fetchWeatherByCity } from "../api/api_weather";
 import CityInput from "./CityInput/CityInput";
 import { WeatherDisplay } from "./WeatherDisplay/WeatherDisplay";
 import Forecast from "./WeatherDisplay/Forecast";
+import Skeleton from "./Skeleton/Skeleton";
+
 
 const WeatherComponent = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleCitySubmit = async (cityName) => {
     if (cityName.trim() === "") {
@@ -14,21 +17,35 @@ const WeatherComponent = () => {
       setWeatherData(null);
       return;
     }
+    
+    
+    setLoading(true);
+
     try {
+    
+      await new Promise(resolve => setTimeout(resolve, 4000));
+
       const data = await fetchWeatherByCity(cityName);
       setWeatherData(data);
       setError(null);
     } catch (error) {
       setError("City is not Found");
       setWeatherData(null);
+    } finally {
+    
+      setLoading(false);
     }
   };
 
   return (
     <div className="flex flex-col justify-between gap-10">
       <CityInput onSubmit={handleCitySubmit} />
-      <WeatherDisplay weatherData={weatherData} error={error} />
-      {weatherData && <Forecast cityName={weatherData.name} />}
+      
+      {loading && <Skeleton/>}
+      
+      {!loading && <WeatherDisplay weatherData={weatherData} error={error} />}
+      
+      {!loading && weatherData && <Forecast cityName={weatherData.name} />}
     </div>
   );
 };
